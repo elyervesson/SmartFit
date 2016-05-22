90c354376d5725c782025e4322a08aaef4f62031
Elyervesson de Souza <elyervesson@gmail.com>
2016-05-22 15:09:00 -0300

    Agora vai

--- Servidor/Servidor/settings.py
+++ Servidor/Servidor/settings.py
@@ -13,10 +13,10 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 # Update database configuration with $DATABASE_URL.
-import dj_database_url
+# import dj_database_url
 
 # Update database configuration with $DATABASE_URL.
-DATABASES = {'default': dj_database_url.config()}
+# DATABASES = {'default': dj_database_url.config()}
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
