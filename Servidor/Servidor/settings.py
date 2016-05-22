60fea603ba57ad9d7c6f5a8b71d5d3ffda0010cf
Elyervesson de Souza <elyervesson@gmail.com>
2016-05-22 15:01:31 -0300

    BugFix

--- Servidor/Servidor/settings.py
+++ Servidor/Servidor/settings.py
@@ -16,8 +16,7 @@ import os
 import dj_database_url
 
 # Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASES = {'default': dj_database_url.config()}
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
